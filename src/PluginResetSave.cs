7f22313b6161d2900cf6c666d744ad8548c534fa
jakzo <jack@jf.id.au>
2022-07-01 19:34:20 +1000

    wip

---
+++ src/PluginResetSave.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SpeedrunTools
+{
+  class PluginResetSave : Plugin
+  {
+    private const int SCENE_IDX_MENU = 1;
+
+    public readonly Hotkey HotkeyReset = new Hotkey()
+    {
+      Predicate = (cl, cr) => cl.GetAButton() && cl.GetBButton(),
+      Handler = () =>
+      {
+        MelonLogger.Msg("Resetting save and returning to menu");
+        // Load main menu
+        SceneManager.SetActiveScene(SceneManager.GetSceneAt(SCENE_IDX_MENU));
+      }
+    };
+  }
+}
